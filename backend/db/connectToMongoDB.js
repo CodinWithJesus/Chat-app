cd01d4e16017b96317b5c3802a49558dcffacbd1
CodinWithJesus <106889347+CodinWithJesus@users.noreply.github.com>
2024-02-20 03:10:04 +0530

    front end complete

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,33 +1,12 @@
-import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import mongoose from "mongoose";
 
-const protectRoute = async (req, res, next) => {
+const connectToMongoDB = async () => {
 	try {
-		const token = req.cookies.jwt;
-
-		if (!token) {
-			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
-		}
-
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-		if (!decoded) {
-			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
-		}
-
-		const user = await User.findById(decoded.userId).select("-password");
-
-		if (!user) {
-			return res.status(404).json({ error: "User not found" });
-		}
-
-		req.user = user;
-
-		next();
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log("Connected to MongoDB");
 	} catch (error) {
-		console.log("Error in protectRoute middleware: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
+		console.log("Error connecting to MongoDB", error.message);
 	}
 };
 
-export default protectRoute;
+export default connectToMongoDB;
